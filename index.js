4b451b6417987e23ac0b9d91c900d6efab6c4527
崇志 <chongzhi.ldq@taobao.com>
2018-04-17 10:53:33 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+/**
+ * 请确保在master分支下执行本命令
+ * master分支存的是文档 markdown 源码
+ * gh-pages存的是编译后的文档，对接github pages
+ * 执行 node build 会自动提交master代码并切换到gh-pages分支，删除旧文件，从_book里复制新编译的文件到根目录下
+ */
+
+import { execSync, spawnSync, exec } from 'child_process'
+import path from 'path'
+import fs from 'fs'
+import fse from 'fs-extra'
+import chalk from 'chalk'
+
+const config = {
+    stdio: 'inherit'
+}
+
+/**
+  * 执行单个命令并返回结果字符串
+  */
+function execCommandReturn(command) {
+    return new Promise(function (resolve, reject) {
+
+        let child = exec(command, {
+            maxBuffer: 20000 * 1024
+        })
+        child.stdout.on('data', function (data) {
+            resolve(data)
+        })
+
+        child.on('close', function () {
+            resolve()
+        })
+    })
+}
+
+export default async function (options) {
+    //获取切换分支前的分支名
+    let branchs = await execCommandReturn('git branch')
+    let currentBranch = /.*\*\s([\S]+)\s*/.exec(branchs)[1]
+
+    if (currentBranch !== 'master') {
+        console.log(chalk.red('\n✘ 请在master分支下执行本命令\n'))
+        return
+    }
+
+    console.log(chalk.green(`\nⓘ 开始发布文档到github pages...`))
+
+    spawnSync('gitbook', ['build'], config)
+    spawnSync('git', ['add', '-A'], config)
+    spawnSync('git', ['commit', '-m', '"modify book"'], config)
+    spawnSync('git', ['pull', 'origin', 'master'], config)
+    spawnSync('git', ['push', 'origin', 'master'], config)
+
+    //切换到gh-pages，首次则创建gh-pages分支
+    let rs = spawnSync('git', ['checkout', 'gh-pages'], config)
+    if (rs.status === 1) {
+        spawnSync('git', ['checkout', '-b', 'gh-pages'], config)
+    }
+
+    let files = fs.readdirSync(process.cwd())
+    const notDeleteFiles = ['.git', '_book', 'node_modules']
+    for (const file of files) {
+        if (notDeleteFiles.indexOf(file) === -1) {
+            spawnSync('rm', ['-rf', file])
+        }
+    }
+
+    let copyFiles = fs.readdirSync(path.resolve(process.cwd(), './_book'))
+    for (const file of copyFiles) {
+        fse.copySync(`${process.cwd()}/_book/${file}`, `${process.cwd()}/${file}`)
+    }
+
+    spawnSync('git', ['add', '-A'], config)
+    spawnSync('git', ['commit', '-m', '"publish book"'], config)
+    spawnSync('git', ['pull', 'origin', 'gh-pages'], config)
+    spawnSync('git', ['push', 'origin', 'gh-pages'], config)
+    spawnSync('git', ['checkout', 'master'], config)
+
+    console.log(chalk.green('√ 发布完成 \n'))
+}
